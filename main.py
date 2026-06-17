8adec389d6915c8562f17ffdfb608a756d81388d
Anna Grace Ulses <aulses@uw.edu>
2026-06-17 12:10:32 +0200

    Semi-major axis allocation

--- main.py
+++ main.py
@@ -5,13 +5,32 @@ from pylaplace import LaplaceCoefficient
 
 ####INITIALISE SYSTEM######
 N = 10 #number of planets
-M = 1 #planet mass
-a = 1 #semi-major axes
-e = 
-inner_edge = 0.005
-Ms = #stellar mass
+inner_edge = 0.005 #(AU)
+outer_edge = 1 
+M = 0.5 #planet mass (relative to Mearth)
+
+def hill_sphere(a_i):
+    return rH = a_i*(M/(3*(2*M)))**(1/3) #hill sphere for adjacent planets
+
+a = np.array(N)
+for i,r in enumerate(N): #allocate initial semi-major axes
+    a_innermost = 0.1 #starting semi-major axis
+
+    a[i] = a + 10*hill
+
+    
+
+a =  #semi-major axes (AU)
+e = 0.01
+I = 
+#M,a,e,I allocated for each planet in 1,2..N
+Ms = #stellar mass (relative to Msun)
 
 max_time = 1e9 #evolution time (years)
+
+#####CONSTANTS######
+G = 6.67e-11 #m^3kg^-1s^-2
+
 
 #####ANCILLARY FUNCTIONS#######
 
@@ -32,8 +51,12 @@ def ki_dt(gj, betaj)
 ####MAIN FUNCTIONS#########
 
 def secular_ecc(h,k)
+#big function for:
+#1. calculating eccentricity via secular eccentricity procedure, calles pylaplace to calculate coefficients
+#2. 
 
 def tau_cross(M, nu, del_6, del_ov, P1)
+#calculates time of next crossing (event) as well as the timescales of the events
 
 def crossing_ecc(Mi,Mj, bij, ai, aj)
 
@@ -41,7 +64,8 @@ def event_path(tau_col, tau_scat)
 #evaluates scattering vs collision, gives probability, p_col
 
 def event_outcome(p_col)
-#evaluates new mass, a, and e after either collision or scattering 
+#evaluates new mass, a, and e after either collision or scattering, 
 
 def next_step()
 #re-order planets based on eccentricities after an event, reset time to t = t+tau_cross + event time
+#also evaluate if simulation is done (t = max_time or N = 2 stability criterion satisfied)
