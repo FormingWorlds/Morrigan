796e90f521af3d17e6693f8cfa70406199a8a70a
Anna Grace Ulses <aulses@uw.edu>
2026-06-18 10:37:04 +0200

    Initialise planetary system

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
+import sys
+import os
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pandas as pd 
 from pylaplace import LaplaceCoefficient 
 import pdb 
 
+#####CONSTANTS######
+G = 6.67e-11 #m^3kg^-1s^-2
+M_sun = 1.9892e33 #grams
+M_earth = 5.9736e27 #grams
+
 ####INITIALISE SYSTEM######
 N = 10 #number of planets
 inner_edge = 0.005 #(AU)
-outer_edge = 1 
-M = 0.5 #planet mass (relative to Mearth)
-
-def hill_sphere(a_i):
-    return a_i*(M/(3*(2*M)))**(1/3) #hill sphere for adjacent planets
-
-a = np.empty(N)
-a[0] = 0.1
-for i in range(N): #allocate initial semi-major axes
-    pdb.set_trace
-    a_previous = a[i-1] #starting semi-major axis
-    hill = hill_sphere(a_previous)
-    a[i] = a_previous + 10*hill
-
-pdb.set_trace()
-
 e = 0.01
-#M,a,e,I allocated for each planet in 1,2..N
-Ms = 1 #stellar mass (relative to Msun)
-
+Mp = 0.5 #planet mass (relative to Mearth)
+Ms = 1*M_sun #stellar mass (relative to Msun)
 max_time = 1e9 #evolution time (years)
 
-#####CONSTANTS######
-G = 6.67e-11 #m^3kg^-1s^-2
+def hill_sphere(a_i,M):
+    return a_i * ((2*M) / (3 * Ms))**(1/3) #mutual hill radius for adjacent planets
+
+def allocate_a(M):
+    M *= 5.9736e27 #convert to Earth masses
+    a = np.empty(N)
+    a[0] = 0.1
+    for i in range(1,N): #allocate initial semi-major axes
+        a_previous = a[i-1] #starting semi-major axis
+        hill = hill_sphere(a_previous,M)
+        a[i] = a_previous + 10*hill #planets are spaced about by 10 hill radii
+    return a
+
+plt.scatter(allocate_a(0.1),range(N), label = 'M_p = 0.1')
+plt.scatter(allocate_a(0.5), range(N), label = 'M_p = 0.5')
+plt.scatter(allocate_a(1.0), range(N), label = 'M_p = 1.0')
+plt.legend()
+plt.xlabel('Initial orbital separation (AU)')
+plt.ylabel('Planet number')
+plt.tight_layout()
+plt.savefig('initial_system.png',dpi = 300)
+plt.close()
+
