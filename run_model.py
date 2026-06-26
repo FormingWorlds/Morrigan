556dd8db04928459297996bce2b01c1bd4c45a65
Anna Grace Ulses <aulses@uw.edu>
2026-06-26 14:41:59 +0200

    executable file

---
+++ run_model.py
@@ -0,0 +1,111 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+from pylaplace import LaplaceCoefficient 
+import pdb 
+from astropy.table import Table
+from astropy.io import ascii
+
+from tau_cross.py import * 
+from helper_functions.py import * 
+from merge_embryo.py import * 
+from secular_solution.py import * 
+from crossing_pair.py import * 
+from orbit_cross_K25.py import * 
+from sort_planet.py import *
+
+#####CONSTANTS######
+G = 6.67e-11 #m^3kg^-1s^-2
+M_sun = 1.9892e30 #kg
+M_earth = 5.9736e24 #kg
+
+####VARIABLES TO RUN SIMULATION####
+t = 0.0
+t_ref = 0.0
+t_event = 0.0
+flag_event = 1 
+a_min = 0.005 * 1.5e11 #defines when a planet has fallen into the star 
+max_time = 1e9*365*24*60*60 #evolution time (seconds)
+
+####INITIAL PARAMETERS######
+N = 10 #number of planets
+e = 0.01
+Mp = 0.5*M_earth #planet mass (relative to Mearth)
+Ms = 1*M_sun #stellar mass (relative to Msun)
+rho_p = 5500 #planet density kg/m^3
+np.random.seed(1) #for reproducability
+
+####ALLOCATE PARAMETERS FOR THE SYSTEM###
+def hill_sphere(a_i,M):
+    return a_i * ((M) / (3 * Ms))**(1/3) #mutual hill radius for adjacent planets
+
+def allocate_a(M):
+    a = np.empty(N)
+    a[0] = 0.1 #AU
+    for i in range(1,N): #allocate initial semi-major axes
+        a_previous = a[i-1] #starting semi-major axis
+        hill = hill_sphere(a_previous,M)
+        a[i] = a_previous + 10*hill #planets are spaced out by 10 hill radii
+    return a*1.5e11 #convert to [m] to stay in SI!
+
+def planet_radius(mass,density):
+    return ((3*mass)/(4 * np.pi*density))**(1/3)
+
+#actually initialising system here with arrays for every parameter
+a = allocate_a(Mp)
+ecc = np.full(N,e)
+densities = np.full(N, rho_p)
+masses = np.full(N, Mp)
+live_status = np.ones(N, dtype = bool) #set initial status of planets, all are live by definition at the start
+interact = np.ones(N, dtype = bool) #stores the indices of which planets are participating in an event
+Rp = np.array([planet_radius(i, j) for i,j in zip(masses,densities)])
+
+parameter_names = ['a_AU','e','Mp','Rp','live_status']
+system_information = Table([a/1.5e11,ecc,masses,Rp,live_status],names = parameter_names)
+ascii.write(system_information, 'initial_system.csv', format = 'fixed_width', overwrite = True) #store initial system information
+
+#timestep when not at or during an event
+def time_step(t, t_event):
+    dt = 0.1*(t+1.0e2*365*24*60*60) #timestep in seconds
+    dt = min(dt,abs(t_event - t) + 1.0)
+    return dt
+
+#actually run simulation down here
+while t <= max_time and N > 1:
+    if flag_event == 1: #only recompute secular solution and crossing pair when something has changed
+        a, masses, ecc, Rp, live_status, interact, densities = sort_planet(a, masses, ecc, Rp, live_status, interact, densities)
+        ecc_vec, g, beta = secular_solution(a, masses, ecc, Rp, N)
+        t_ref = t #time for crossing_pair
+
+        icross, t_event = crossing_pair(a, masses, Rp, ecc, ecc_vec, g, beta, interact, N, t, t_ref)
+        flag_event = 0 #event done, do not recalculate secular/crossing otherwise 
+
+    dt = time_step(t, t_event)
+    t += dt #adjust time to account for event
+    
+    #propagate secular eccentricities
+    h_t = np.zeros(N)
+    k_t = np.zeros(N)
+    for i in range(N):
+        h_t[i] = np.sum(ecc_vec[i, :] * np.sin(g * (t - t_ref) + beta)) # eq A10
+        k_t[i] = np.sum(ecc_vec[i, :] * np.cos(g * (t - t_ref) + beta)) 
+    ecc = np.sqrt(h_t**2 + k_t**2) # eq 3 finally!
+    
+    #check for crossings/close encounters
+    if t >= t_event:
+        flag_event = 1
+        orbit_cross_K25(a, masses, Rp, ecc, interact, live_status, N, icross)
+    
+    #update planet radius
+    Rp = np.array([planet_radius(masses[i], densities[i]) for i in range(N)])
+    
+    #remove planets too close to the star
+    for i in range(N):
+        if (1.0 - ecc[i]) * a[i] < a_min:
+            live[i] = False
+            flag_event = 1
+
+
+#save final system information
+system_information = Table([a/1.5e11, ecc, masses, Rp, live_status], names=parameter_names)
+ascii.write(system_information, 'final_system.csv', format='fixed_width', overwrite=True)
